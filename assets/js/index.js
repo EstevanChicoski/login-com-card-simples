c550cb888636fb2cf3367330d51dc3e57f95b594
Jeall <111143583+EstevanChicoski@users.noreply.github.com>
2022-09-17 14:56:08 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,8 @@ function calculate(){
     sobrenome = document.getElementById("sobrenome").value
     nomeCompleto = (nome + " " + sobrenome)
 
-    document.getElementById("nameUser").innerHTML = nomeCompleto
+    document.getElementById("nameUser").innerHTML = (nomeCompleto)
     document.getElementById("recado").innerHTML = ("Olá " + nomeCompleto + "! Seja bem vindo ao nosso site, aqui você poderá fazer compras e saber de detalher de eventos e muito mais!")
     document.getElementById("nameMenu").innerHTML = ("| " + nomeCompleto)
+
 }
